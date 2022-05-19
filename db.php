cb46a6b47ce180de12ae958cf790a71815fa801b
hemantmudgal <hemant.mudgal_cs19@gla.ac.in>
2022-05-19 18:22:39 +0530

    update changes

--- db.php
+++ db.php
@@ -5,11 +5,9 @@ protected $connection;
 function setconnection(){
     try{
         $this->connection=new PDO("mysql:host=localhost; dbname=library_managment","root","");
-        // echo "Done";
+
     }catch(PDOException $e){
         echo "Error";
-        //die();
-
     }
 }
 
